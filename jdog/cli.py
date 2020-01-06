84971af23cdae3713bdc4da3305e09a322c97e8b
Petr Nymsa <nymsa.petr@outlook.cz>
2020-01-06 21:13:40 +0100

    add lang option

--- jdog/cli.py
+++ jdog/cli.py
@@ -1,21 +1,38 @@
 import click
 import json
-import os
 from . import Jdog
+from faker.config import AVAILABLE_LOCALES
 
 
-def validate_scheme(_, __, value):
-    if os.path.exists(value):
-        return value
+def callback_lang_help(ctx, prop, value):
+    if not value:
+        return
+    click.echo('Use one of the following language code: ')
+    click.echo(AVAILABLE_LOCALES)
+    ctx.exit()
 
-    raise click.BadArgumentUsage(f'Provided scheme path {value} does not exists')
+
+
+def validate_lang(ctx, prop, value):
+    if value not in AVAILABLE_LOCALES:
+        raise click.BadParameter('Language code is invalid. See --lang-help for accepted values')
+
+    return value
+
+# developer note: Although we cane use for -l parameter click.Choice,
+# there is not way to truncate / or hide all options and its look ugly
+# so I have decided to introduce special option --lang-help to show and do manual validation for -l option
 
 
 @click.command('jdog')
-@click.argument('scheme', callback=validate_scheme)
+@click.argument('scheme', type=click.Path(exists=True))
 @click.option('-p', '--pretty', is_flag=True, default=False, help='Output pretty JSON')
-def run(scheme, pretty):
-    jdog = Jdog()
+@click.option('-l', '--lang', default='en-US', help='Language to use', callback=validate_lang)
+@click.option('--lang-help', is_flag=True, default=False, help='Displays available language codes and exit.', callback=callback_lang_help)
+def run(scheme, pretty, lang, lang_help):
+    """Accepts SCHEME and generate new data to stdin or to specified OUTPUT"""
+
+    jdog = Jdog(lang)
 
     with open(scheme, "r") as f:
         scheme_text = f.read()
