fb0c3e8ecf284092cdf32fb93147163deed30723
Petr Nymsa <nymsa.petr@outlook.cz>
2020-01-08 09:54:20 +0100

    add cli test

---
+++ tests/test_cli.py
@@ -0,0 +1,35 @@
+from click.testing import CliRunner
+from jdog.cli import run
+
+
+def test_no_scheme():
+    runner = CliRunner()
+    res = runner.invoke(run)
+
+    assert res.exit_code != 1
+    assert 'Missing argument "SCHEME"' in res.stdout
+
+
+def test_scheme():
+    scheme = '{"value":"foo"}'
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        with open('scheme.json', 'w') as f:
+            f.write(scheme)
+
+        result = runner.invoke(run, ['scheme.json'])
+        assert result.exit_code == 0
+        assert result.output == f'{scheme}\n'
+
+
+def test_scheme_pretty():
+    scheme = '{"value":"foo"}'
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        with open('scheme.json', 'w') as f:
+            f.write(scheme)
+
+        result = runner.invoke(run, ['scheme.json', '-p'])
+        assert result.exit_code == 0
+        assert result.output == """{\n    "value": "foo"\n}\n"""
+
