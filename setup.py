d10d866ca18487775ed02c69fb9d91d7f1711d2f
Petr Nymsa <nymsa.petr@outlook.cz>
2020-01-10 00:21:16 +0100

    bump up version to 0.2.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst', encoding='utf-8') as f:
 
 setup(
     name='jdog',
-    version='0.2.1',
+    version='0.2.2',
     description='Just another Data Offline Generator',
     long_description=long_description,
     author='Petr Nymsa',
