aec7253a57a2c66968304d389fb671c70790bf21
Petr Nymsa <nymsa.petr@outlook.cz>
2020-01-11 10:59:11 +0100

    jdog add_matcher api accepts regex pattern
    This is a minor breaking change which introduces more cleaner api to create new placeholder.

--- tests/test_jdog.py
+++ tests/test_jdog.py
@@ -33,11 +33,8 @@ def test_simple_placeholder(monkeypatch):
 def test_add_new():
     scheme = """{"f":"{{test}}"}"""
 
-    def match(token):
-        return re.match(r'^{{test}}$', token)
-
     jdog = jd.Jdog()
-    jdog.add_matcher('test', match, lambda tok,arg: FuncStrPlaceholder(tok,arg, lambda _: 'foo'))
+    jdog.add_matcher('test', r'^{{test}}$', lambda tok, arg: FuncStrPlaceholder(tok, arg, lambda _: 'foo'))
 
     jdog.parse_scheme(scheme)
     res = jdog.generate()
@@ -47,12 +44,8 @@ def test_add_new():
 
 def test_redefine():
     scheme = """{"f":"{{age}}"}"""
-
-    def match(token):
-        return re.match(r'^{{age}}$', token)
-
     jdog = jd.Jdog()
-    jdog.add_matcher('age', match, lambda tok,arg: FuncPlaceholder(tok,arg, lambda _: 666))
+    jdog.add_matcher('age', r'^{{age}}$', lambda tok, arg: FuncPlaceholder(tok, arg, lambda _: 666))
 
     jdog.parse_scheme(scheme)
     res = jdog.generate()
